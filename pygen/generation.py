6f2a334cff8b60dcc5ac22efe94c9ab3a9737485
Akil Marshall <akilmarshall@gmail.com>
2022-06-09 14:51:16 -0700

    extended logging capabilites

--- pygen/generation.py
+++ pygen/generation.py
@@ -5,6 +5,7 @@ from collections import deque
 from os import mkdir
 from os.path import exists
 from shutil import rmtree
+import matplotlib.pyplot as plt
 
 
 class Image:
@@ -84,28 +85,54 @@ class Image:
         return self.tis.to_image(self.img)
 
 
-def generate(n: int, m: int, tis: TIS):
+def generate(n: int, m: int, tis: TIS, verbose:bool, log:bool):
     active = deque()
     active.append(Image(n, m, tis))
+    if log:
+        pop_history = []
+        img_done = []  # a list of time steps when images terminated
+        _time_step = 0
+    if verbose:
+        i = 0
+        ith = 0
     while len(active) > 0:
-        print(len(active))
+        if verbose:
+            if i % 1000:
+                print(len(active))
+            i += 1
+        if log:
+            pop_history.append(len(active))
         img = active.popleft()
         x, y = img.min_entropy()
         for t in img[x][y]:
             fork = img.copy()
             fork.collapse(x, y, t)
             if fork.complete():
+                if log:
+                    img_done.append(_time_step)
+                if verbose:
+                    print(f'{ith}.png')
+                    ith += 1
                 yield fork
             else:
                 active.append(fork)
+        if log:
+            _time_step += 1
+    if log:
+        plt.plot(pop_history)
+        plt.vlines(img_done, 0, max(pop_history) * 0.25, colors='r')
+        plt.ylabel('# of possible images being collapsed')
+        plt.xlabel('t')
+        plt.title(f'{n}x{m} Population over lifetime of Generation algorithm')
+        plt.savefig(f'{n}x{m} population plot.png')
 
 
-def sudoku_dump(n: int, m: int, tis: TIS, path: str):
+def sudoku_dump(n: int, m: int, tis: TIS, path: str, verbose=False, log=False):
     if exists(path):
         rmtree(path)
     mkdir(path)
     mkdir(f'{path}/partial')
-    for i, img in enumerate(generate(n, m, tis)):
+    for i, img in enumerate(generate(n, m, tis, verbose, log)):
         out = img.to_image()
         if img.good():
             out.save(f"{path}/{i}.png")
