a16ebd562813d41498d103622fcb9e77d073106e
Akil Marshall <akilmarshall@gmail.com>
2022-06-19 14:51:24 -0700

    added util module for misc functions

---
+++ src/util.rs
@@ -0,0 +1,13 @@
+use std::fs::{create_dir_all, remove_dir_all};
+use std::path::Path;
+
+pub fn mkdir(p: &String) {
+    // check if path exists
+    // true -> recursive delete
+    // make new directory at path
+    let path = Path::new(p).to_path_buf();
+    if path.is_dir() {
+        remove_dir_all(path.clone()).ok();
+    }
+    create_dir_all(path).ok();
+}
